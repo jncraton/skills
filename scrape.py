05a552fd9f8b2f5f46812d3236dc35d0fe2568b6
Jon Craton <jncraton@gmail.com>
2026-01-23 20:11:16 -0500

    Strip links

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ from markdownify import markdownify
 
 html = urllib.request.urlopen(sys.argv[1]).read().decode('utf8')
 main = re.search(r"<main.*?</main>", html, flags=re.I|re.M|re.DOTALL)[0]
-md = markdownify(main)
+md = markdownify(main, strip=['a'])
 
 with open(sys.argv[2], 'w') as f:
     f.write(md)
