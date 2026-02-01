2d8f6a83df2e1297ba0470254f5e3bf4b0dfa9d0
Jon Craton <jncraton@gmail.com>
2026-02-01 14:47:59 -0500

    Symlink opencode config

--- setup-opencode.sh
+++ setup-opencode.sh
@@ -2,4 +2,5 @@
 # Create symlinks within opencode configs
 
 ln -s $(realpath AGENTS.md) ~/.config/opencode/AGENTS.md
+ln -s $(realpath opencode.json) ~/.config/opencode/opencode.json
 ln -s $(realpath skills) ~/.config/opencode/skills
