f3234dc2b53179c79f6bba859b5147add23d1fe2
Jon Craton <jncraton@gmail.com>
2026-02-02 09:05:24 -0500

    Allow multiple image queries to be merged

--- skills/creating-lecture/scripts/related_content.py
+++ skills/creating-lecture/scripts/related_content.py
@@ -9,36 +9,41 @@ def clean_html(text):
     return re.sub(r"<[^>]*>", "", text).replace("\n", " ")
 
 
-def search_images(query):
-    keywords = ' '.join(query.split())
-    params = {
-        "action": "query",
-        "format": "json",
-        "prop": "imageinfo",
-        "iiprop": "url|extmetadata",
-        "iiurlwidth": 500,
-        "generator": "search",
-        "gsrsearch": f"{keywords} incategory:Quality_images",
-        "gsrlimit": 50,
-        "gsrnamespace": 6,
-    }
-    url = f"https://commons.wikimedia.org/w/api.php?{urllib.parse.urlencode(params)}"
-    req = urllib.request.Request(url, headers={"User-Agent": "ImageSearchScript/1.0"})
+def search_images(queries):
     print("## Images\n")
-    with urllib.request.urlopen(req) as response:
-        data = json.loads(response.read().decode())
-        pages = data.get("query", {}).get("pages", {})
-        for page in pages.values():
-            title = page.get("title", "")
-            imageinfo = page.get("imageinfo", [{}])[0]
-            thumb_url = imageinfo.get("thumburl")
-            if thumb_url:
-                metadata = imageinfo.get("extmetadata", {})
-                description = metadata.get("ImageDescription", {}).get("value", title)
-                description = clean_html(description)
-                if len(description) > 100:
-                    print(f"{thumb_url} {description[:500]}")
-
+    for query in queries:
+        print(f"### {query}\n")
+        params = {
+            "action": "query",
+            "format": "json",
+            "prop": "imageinfo",
+            "iiprop": "url|extmetadata",
+            "iiurlwidth": 500,
+            "generator": "search",
+            "gsrsearch": f"{query} incategory:Quality_images",
+            "gsrlimit": 10,
+            "gsrnamespace": 6,
+        }
+        url = f"https://commons.wikimedia.org/w/api.php?{urllib.parse.urlencode(params)}"
+        req = urllib.request.Request(url, headers={"User-Agent": "ImageSearchScript/1.0"})
+        count = 0
+        with urllib.request.urlopen(req) as response:
+            data = json.loads(response.read().decode())
+            pages = data.get("query", {}).get("pages", {})
+            for page in pages.values():
+                title = page.get("title", "")
+                imageinfo = page.get("imageinfo", [{}])[0]
+                thumb_url = imageinfo.get("thumburl")
+                if thumb_url:
+                    metadata = imageinfo.get("extmetadata", {})
+                    description = metadata.get("ImageDescription", {}).get("value", title)
+                    description = clean_html(description)
+                    if len(description) > 30:
+                        print(f"{thumb_url} {description[:500]}")
+                        count += 1
+                        if count >= 3:
+                            break
+            print()
 
 def search_quotes(query):
     quote_path = Path(__file__).parent.parent / "references/quotes.txt"
@@ -60,7 +65,7 @@ def search_quotes(query):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        search_images(' '.join(sys.argv[1:]))
+        search_images(sys.argv[1:])
         print()
         search_quotes(' '.join(sys.argv[1:]))
 
